85ffdc5e58cfce6c289b285b74f8d386ad720d65
babciaexe <94741425+babciaexe@users.noreply.github.com>
2021-11-21 11:53:28 +0100

    Ex1
    Car, fuel task

---
+++ Ex1/ex1/ex1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine(CalculateAvarageConsumption());
+
+                Console.Write("Press <C> to continue or <X> to exit... ");
+                if(Console.ReadKey().Key == ConsoleKey.X)
+                {
+                    break;
+                }
+                Console.WriteLine("\n");
+            }
+
+
+        }
+        static string CalculateAvarageConsumption()
+        {
+            string message;
+            Console.WriteLine("Type number of travbeled km");
+            var traveledInput = Console.ReadLine();
+            Console.WriteLine("Type number of used fuel in liters");
+            var usedFuelInput = Console.ReadLine();
+            
+            if (float.TryParse(traveledInput, out float traveled ) && float.TryParse(usedFuelInput, out float usedFuel))
+            {
+                float averageConsumption = (traveled / 100) / usedFuel ;
+                message = $"{averageConsumption} Liters per 100km";
+            }
+            else 
+            {
+                message = "Please use correct info";
+            }
+            return (message);
+        }
+    }
+}
